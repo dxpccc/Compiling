eee576e5e00de2f1cabcbd2c0b5b156842932678
dxpccc <1729824616@qq.com>
2021-10-07 16:55:24 +0800

    2.0重构

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,24 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.security.Key;
+import java.util.Calendar;
 import java.util.StringTokenizer;
 
 public class Main {
     public static boolean isStop = false;
+    public static StringBuilder token = new StringBuilder();
+    public static char curChar;
 
     public static void main(String[] args) {
         File file = new File(args[0]);
         String str;
+        int index;
         try {
             FileReader reader = new FileReader(file);
             BufferedReader bfReader = new BufferedReader(reader);
             while ((str = bfReader.readLine()) != null) {
-                System.out.println(str);
                 print(str);
-                if (isStop)
-                    break;
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -24,7 +26,74 @@ public class Main {
     }
 
     public static void print(String string) {
-        StringTokenizer st = new StringTokenizer(string, " \n\r\t=;(){}+*/<>", true);
+        token.delete(0, token.length());
+        int len = string.length();
+        for (int index = 0; index < len; ++index) {
+            curChar = string.charAt(index);
+            if (isNonDigit(curChar)) {
+                while (isNonDigit(curChar) || isDigit(curChar)) {
+                    token.append(curChar);
+                    ++index;
+                    if (index < len)
+                        curChar = string.charAt(index);
+                    else
+                        break;
+                }
+                --index;
+                String str = token.toString();
+                if (Keyword.isKeyword(string))
+                    Keyword.print(string);
+                else
+                    System.out.println("Ident(" + str + ")");
+                token.delete(0, token.length());
+            } else if (isDigit(curChar)) {
+                while (isDigit(curChar)) {
+                    token.append(curChar);
+                    ++index;
+                    if (index < len)
+                        curChar = string.charAt(index);
+                    else
+                        break;
+                }
+                --index;
+                String str = token.toString();
+                System.out.println("Number(" + str + ")");
+                token.delete(0, token.length());
+            } else if (Separator.isSeparator(curChar)) {
+                Separator.print(curChar);
+            } else if (Separator.isEqual(curChar)) {
+                if (index < len -1 && string.charAt(++index) == '=') {
+                    System.out.println("Eq");
+                } else
+                    Separator.print(curChar);
+            } else if (isBlank(curChar)) {
+                while (isBlank(curChar)) {
+                    ++index;
+                    if (index < len)
+                        curChar = string.charAt(index);
+                    else
+                        break;
+                }
+                --index;
+            } else
+                System.out.println("Err");
+        }
+    }
+
+    public static boolean isNonDigit(char ch) {
+        return Character.isLetter(ch) || ch == '_';
+    }
+
+    public static boolean isDigit(char ch) {
+        return Character.isDigit(ch);
+    }
+
+    public static boolean isBlank(char ch) {
+        return ch == ' ' || ch == '\n' || ch == '\r' || ch == '\t';
+    }
+
+    /*public static void print(String string) {
+        StringTokenizer st = new StringTokenizer(string, " \n\r\t=;(){}/+*<>", true);
         while (st.hasMoreTokens()) {
             String a = st.nextToken();
             if (a.equals("=") && st.hasMoreTokens()) {
@@ -64,5 +133,5 @@ public class Main {
                 //isStop = true;
             }
         }
-    }
+    }*/
 }
--- src/Separator.java
+++ src/Separator.java
@@ -1,71 +1,66 @@
 public class Separator {
-    public static boolean isSeparator(String string) {
+    public static boolean isSeparator(char ch) {
         boolean res = false;
-        if (string != null) {
-            switch (string) {
-                case "=":
-                case ";":
-                case "(":
-                case ")":
-                case "{":
-                case "}":
-                case "+":
-                case "*":
-                case "/":
-                case "<":
-                case ">":
-                case "==":
+            switch (ch) {
+                case ';':
+                case '(':
+                case ')':
+                case '{':
+                case '}':
+                case '+':
+                case '*':
+                case '/':
+                case '<':
+                case '>':
                     res = true;
                     break;
                 default:
                     break;
             }
-        }
         return res;
     }
 
-    public static void print(String string) {
-        if (string != null) {
-            switch (string) {
-                case "=":
-                    System.out.println("Assign");
-                    break;
-                case ";":
-                    System.out.println("Semicolon");
-                    break;
-                case "(":
-                    System.out.println("LPar");
-                    break;
-                case ")":
-                    System.out.println("RPar");
-                    break;
-                case "{":
-                    System.out.println("LBrace");
-                    break;
-                case "}":
-                    System.out.println("RBrace");
-                    break;
-                case "+":
-                    System.out.println("Plus");
-                    break;
-                case "*":
-                    System.out.println("Mult");
-                    break;
-                case "/":
-                    System.out.println("Div");
-                    break;
-                case "<":
-                    System.out.println("Lt");
-                    break;
-                case ">":
-                    System.out.println("Gt");
-                    break;
-                case "==":
-                    System.out.println("Eq");
-                    break;
-                default:
-                    break;
-            }
+    public static boolean isEqual(char ch) {
+        return ch == '=';
+    }
+
+    public static void print(char ch) {
+        switch (ch) {
+            case '=':
+                System.out.println("Assign");
+                break;
+            case ';':
+                System.out.println("Semicolon");
+                break;
+            case '(':
+                System.out.println("LPar");
+                break;
+            case ')':
+                System.out.println("RPar");
+                break;
+            case '{':
+                System.out.println("LBrace");
+                break;
+            case '}':
+                System.out.println("RBrace");
+                break;
+            case '+':
+                System.out.println("Plus");
+                break;
+            case '*':
+                System.out.println("Mult");
+                break;
+            case '/':
+                System.out.println("Div");
+                break;
+            case '<':
+                System.out.println("Lt");
+                break;
+            case '>':
+                System.out.println("Gt");
+                break;
+            default:
+                break;
         }
     }
 }
