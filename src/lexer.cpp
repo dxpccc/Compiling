578f310b877d05bc136fbc62537f48962e08288e
dpp <1729824616@qq.com>
2021-11-14 11:14:46 +0800

    fix: remove some files

---
+++ src/lexer.cpp
@@ -0,0 +1,7 @@
+#include "lexer.hpp"
+
+using namespace Compiler;
+
+string Lexer::get_token(FILE *file)
+{
+}
---
+++ src/token.hpp
@@ -0,0 +1,20 @@
+#ifndef TOKEN_H_
+#define TOKEN_H_
+
+enum class TokenType
+{
+    TOKEN_EOF = 0,
+    TOKEN_NUMBER,
+    TOKEN_INT,
+    TOKEN_MAIN,
+    TOKEN_RETURN,
+    TOKEN_PAREN_L,
+    TOKEN_PAREN_R,
+    TOKEN_BRACE_L,
+    TOKEN_BRACE_R,
+    TOKEN_SEMICOLON,
+    TOKEN_PLUS,
+    TOKEN_MINUS,
+};
+
+#endif
