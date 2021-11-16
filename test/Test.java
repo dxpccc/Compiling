d80b926d03e0980d1fa481a032d966b23b33f838
dpp <1729824616@qq.com>
2021-11-17 01:28:50 +0800

    fix: 修复lab1bug

--- test/Test.java
+++ test/Test.java
@@ -4,10 +4,11 @@ import mid.IRBuilder;
 import util.AST.CompUnitAST;
 import util.Token;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Test {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Lexer lexer = new Lexer("./test/test.txt");
         ArrayList<Token> res_lexer = lexer.analyse();   // 词法分析结果
         if (res_lexer == null)                                  // 词法分析出错返回-1
@@ -20,5 +21,17 @@ public class Test {
 
         IRBuilder builder = new IRBuilder("./test/output.txt");
         builder.generateIR(res_parser);
+    }*/
+
+    public static void main(String[] args) {
+        try {
+            Reader reader = new FileReader(args[0]);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String s;
+            while ((s = bufferedReader.readLine()) != null)
+                System.out.println(s);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
